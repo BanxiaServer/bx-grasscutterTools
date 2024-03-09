965d26ef8f4bd58bc1365ab7aba15986d5aca54b
亡灵暴龙大帝 <q200608233416@163.com>
2024-03-09 23:50:42 +0800

    修改部分文字

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -52,14 +52,14 @@ const LoginHS = ref(true)
         <div class="px-4 py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10">
           <div class="relative flex items-center text-2xl sm:text-2xl font-blimone">
             <router-link to="/" class="mr-3 flex-none w-[2.0625rem] md:w-auto leading-6 dark:text-slate-200">
-              GrasscutterTools</router-link>
+              半夏|GrasscutterTools4.0|</router-link>
             <div class="relative items-center hidden ml-auto lg:flex">
               <nav class="text-sm font-semibold leading-6 text-slate-700 dark:text-slate-200">
                 <ul class="flex space-x-8">
                   <li>
-                    <a href="http://bxgov.cn:21480/" target="_bank"
+                    <a href="https://www.grasscutter.cf/" target="_bank"
                       class="block ml-6 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
-                      用户后台
+                      Grasscutter-docs-zh_CN
                     </a>
                   </li>
                   <li v-if="LoginHS">
