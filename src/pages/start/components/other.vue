9cebf90b3833759f9d92a5cf0b988a5d6386d720
亡灵暴龙大帝 <q200608233416@163.com>
2024-03-18 17:18:14 +0800

    更改快捷指令为常用指令 选择方式为下拉框

--- src/pages/start/components/other.vue
+++ src/pages/start/components/other.vue
@@ -8,92 +8,12 @@ import other from './json/other.json'
 
 const { text, isSupported, copy } = useClipboard()
 
-const options = reactive([
-  {
-    title: '加速*3倍',
-    value: '/w time3',
-  },
-  {
-    title: '点亮地图',
-    value: '/prop unlockmap 1',
-  },
-  {
-    title: '解锁所有状态',
-    value: '/unlockall',
-  },
-  {
-    title: '解锁风之翼',
-    value: '/prop fly on',
-  },
-  {
-    title: '无敌',
-    value: '/prop god on',
-  },
-  {
-    title: '无限体力',
-    value: '/prop ns on',
-  },
-  {
-    title: '无限能量',
-    value: '/prop ue on',
-  },
-  {
-    title: '无cd',
-    value: '/setstats cdr 100%',
-  },
-  {
-    title: '满血',
-    value: '/h',
-  },
-  {
-    title: '设置纪行等级',
-    value: '/prop bp 50',
-  },
-  {
-    title: '世界等级8',
-    value: '/prop wl 8',
-  },
-  {
-    title: '全队充能',
-    value: '/spawn 2008 25',
-  },
-  {
-    title: '查看坐标',
-    value: '/pos',
-  },
-  {
-    title: '一万纠缠',
-    value: '/give 223 x10000',
-  },
-  {
-    title: '击杀全部怪物',
-    value: '/killall',
-  },
-  {
-    title: '自杀',
-    value: '/kill 0',
-  },
-  {
-    title: '快速重进游戏',
-    value: '/kick',
-  },
-  {
-    title: '清空角色命座(需重登)',
-    value: '/resetconst',
-  },
-  {
-    title: '进入多人游戏状态',
-    value: '/coop',
-  },
-  {
-    title: '[多人游戏]全体传送',
-    value: '/tpall',
-  },
-  {
-    title: '起飞',
-    value: '/tp ~ ~100 ~',
-  },
-])
+var value2 = ref("/w time3")
+
+const value = computed(() => {
+  return `${value2.value}`
+})
+const options = reactive(other)
 const message = Message
 
 function copyvalue() {
